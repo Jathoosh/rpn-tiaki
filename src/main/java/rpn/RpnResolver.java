7069be3045952e7a20b8cafd1fccf50722a20e4b
Daniel Aguiar <daniel.aguiar@outlook.fr>
2023-11-08 16:53:07 +0100

    remove main function

--- src/main/java/rpn/RpnResolver.java
+++ src/main/java/rpn/RpnResolver.java
@@ -6,16 +6,6 @@ import java.util.*;
 import java.util.stream.Collectors;
 
 public class RpnResolver {
-    public static void main(String[] args) {
-
-        System.out.println("Enter RPN expression: ");
-        Scanner scanner = new Scanner(System.in);
-
-        String input = scanner.nextLine();
-
-        int result = resolve(input);
-
-    }
 
     public static Integer resolve(String input) {
         List<String> tokens = Arrays.stream(
